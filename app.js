3644ac5ab512d0ba9e5d7a93f86a25b159c6c8cf
KIMCOYG <kjhkjh9753@gmail.com>
2020-03-08 00:07:01 +0900

    webpack 완료

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(helmet());
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static")); //static으로 가려고 하면 static 폴더를 보게 함
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true})); //html form
