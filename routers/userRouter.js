8d7c14323aa6ffc765c34df8e620661aec4cf908
KIMCOYG <kjhkjh9753@gmail.com>
2020-03-09 00:25:48 +0900

    User Profile Update

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
 import routes from "../routes";
-import { userDetail, editProfile, changePassword } from "../controller/userController";
+import { userDetail, changePassword, getEditProfile } from "../controller/userController";
 import { onlyPrivate } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
 userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
