4df22056675fd7498a8c8bee4f4a27a30ca7c957
KIMCOYG <kjhkjh9753@gmail.com>
2020-03-22 12:58:14 +0900

    댓글 작성

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,5 +1,6 @@
 import routes from "../routes";
 import Video from "../models/Video";
+import Comment from "../models/Comment";
 
 export const home = async (req, res) => {
   try{
@@ -51,8 +52,10 @@ export const videoDetail = async (req, res) => {
   } = req;
 
   try {
-    const video = await Video.findById(id).populate("creator"); //populate 객채를 불러오는 함수 오직 objectID 타입에만 사용 가능
-    // console.log(video);
+    // const video = await Video.findById(id).populate("creator"); //populate 객채를 불러오는 함수 오직 objectID 타입에만 사용 가능
+    const video = await Video.findById(id)
+      .populate("creator")
+      .populate("comments");
     res.render("videoDetail", { pageTitle: video.title, video });
   } catch(error){
     console.log(error);
@@ -123,3 +126,25 @@ export const postRegisterView = async (req, res) => {
   }
 };
 
+// Add Comment
+
+export const postAddComment = async (req, res) => {
+  const {
+    params: { id },
+    body: { comment },
+    user
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    const newComment = await Comment.create({
+      text: comment,
+      creator: user.id
+    });
+    video.comments.push(newComment.id);
+    video.save();
+  } catch (error) {
+    res.status(400);
+  } finally {
+    res.end();
+  }
+};
